285a766ffbacc6b2690130c7e32859ad455caec3
Yukinamida-git <62280710+Yukinamida-git@users.noreply.github.com>
2020-05-28 14:20:10 +0200

    Add files via upload

---
+++ csm/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <link  rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Campus Sud des Métiers</title>
+</head>
+<body>
+    <h1>Campus Sud des Métiers</h1>
+</body>
+</html>
