bebae4dc33818ea642b9c7d2d33a2c2d838ac5e9
Yukinamida <deliquaire.nagui@live.Fr>
2020-05-28 15:16:54 +0200

    1st try

--- index.php
+++ index.php
@@ -1,12 +1,21 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-    <link  rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
+    <link  rel="stylesheet" href="<?php echo get_stylesheet_uri();?>">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Campus Sud des Métiers</title>
+    <title><?php bloginfo('name');?> - <?php bloginfo('description');?></title>
 </head>
 <body>
-    <h1>Campus Sud des Métiers</h1>
+    <main>
+        <h1><?php bloginfo('description');?></h1>
+            <?php while(have_posts()) : the_post();?>
+                <article>
+                    <h2><?php the_title();?></h2>
+                    <?php the_post_thumbnail('medium'); ?>
+                    <?php the_content();?>
+                </article>
+            <?php endwhile; ?>
+    </main>
 </body>
 </html>
